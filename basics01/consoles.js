5d096974782f0d803c9f91ef62ec7241f3ac8ae2
naveen1845 <148039867+naveen1845@users.noreply.github.com>
2024-01-26 09:24:17 +0000

    variables and consoles

---
+++ basics01/consoles.js
@@ -0,0 +1,3 @@
+console.log("naveen the boss");
+console.error("naveen the error")
+console.warn("naveen the warn")
---
+++ basics01/variables.js
@@ -0,0 +1,8 @@
+// variables -> let
+// constants -> const
+
+let username = "naveen"
+const email = "naveenpattihal@gmail.com"
+
+console.log(username);
+console.log(email);
