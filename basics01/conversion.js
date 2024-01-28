870e6dcb2896dbf8baac1a8abcb4b09500683658
naveen1845 <148039867+naveen1845@users.noreply.github.com>
2024-01-28 14:35:52 +0000

    add conversion

---
+++ basics01/conversion.js
@@ -0,0 +1,12 @@
+let value = '100'
+
+let valueInt = parseInt(value)  //explicit conversion
+
+console.log(value);
+console.log(typeof value);
+
+
+
+
+console.log(5 + undefined);  // => NaN
+console.log(5 + null);       // => 5
